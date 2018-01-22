817109723d84a32309405e32d5d3e37ec952023a
Jiri Pechanec <jpechane@redhat.com>
2018-01-22 14:33:03 +0100

    DBZ-541 Fix behaviour for all stables history

--- debezium-connector-mysql/src/main/java/io/debezium/connector/mysql/MySqlSchema.java
+++ debezium-connector-mysql/src/main/java/io/debezium/connector/mysql/MySqlSchema.java
@@ -334,7 +334,7 @@ public class MySqlSchema {
                 // schema change records so that failure recovery (which is based on of the history) won't lose
                 // schema change records.
                 try {
-                    if (changes.stream().anyMatch(filters().tableFilter()::test)) {
+                    if (!storeOnlyMonitoredTablesDdl || changes.stream().anyMatch(filters().tableFilter()::test)) {
                         dbHistory.record(source.partition(), source.offset(), databaseName, tables, ddlStatements);
                     } else {
                         logger.debug("Changes for DDL '{}' were filtered and not recorded in schema history", ddlStatements);
