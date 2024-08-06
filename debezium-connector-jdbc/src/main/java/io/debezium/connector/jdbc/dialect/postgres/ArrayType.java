bpaquet/debezium-connector-jdbc

DBZ-7752: Handle array for postgres

src/main/java/io/debezium/connector/jdbc/dialect/postgres/ArrayType.java
@@ -1,0 +1,49 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.connector.jdbc.dialect.postgres;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.kafka.connect.data.Schema;
+
+import io.debezium.connector.jdbc.ValueBindDescriptor;
+import io.debezium.connector.jdbc.dialect.DatabaseDialect;
+import io.debezium.connector.jdbc.type.AbstractType;
+import io.debezium.connector.jdbc.type.Type;
+
+/**
+ * An implementation of {@link Type} for {@code ARRAY} column types.
+ *
+ * @author Bertrand Paquet
+ */
+
+public class ArrayType extends AbstractType {
+
+    public static final ArrayType INSTANCE = new ArrayType();
+
+    private String typeName;
+
+    @Override
+    public String[] getRegistrationKeys() {
+        return new String[]{ "ARRAY" };
+    }
+
+    @Override
+    public String getTypeName(DatabaseDialect dialect, Schema schema, boolean key) {
+        Type subType = dialect.getSchemaType(schema.valueSchema());
+        typeName = subType.getTypeName(dialect, schema.valueSchema(), key);
+        return typeName + "[]";
+    }
+
+    @Override
+    public List<ValueBindDescriptor> bind(int index, Schema schema, Object value) {
+        if (value == null) {
+            return Arrays.asList(new ValueBindDescriptor(index, null));
+        }
+        return List.of(new ValueBindDescriptorArray(index, value, java.sql.Types.ARRAY, typeName));
+    }
+}

jpechane/debezium

debezium-connector-jdbc/src/main/java/io/debezium/connector/jdbc/dialect/postgres/ArrayType.java
@@ -1,0 +1,49 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.connector.jdbc.dialect.postgres;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.kafka.connect.data.Schema;
+
+import io.debezium.connector.jdbc.ValueBindDescriptor;
+import io.debezium.connector.jdbc.dialect.DatabaseDialect;
+import io.debezium.connector.jdbc.type.AbstractType;
+import io.debezium.connector.jdbc.type.Type;
+
+/**
+ * An implementation of {@link Type} for {@code ARRAY} column types.
+ *
+ * @author Bertrand Paquet
+ */
+
+public class ArrayType extends AbstractType {
+
+    public static final ArrayType INSTANCE = new ArrayType();
+
+    private String typeName;
+
+    @Override
+    public String[] getRegistrationKeys() {
+        return new String[]{ "ARRAY" };
+    }
+
+    @Override
+    public String getTypeName(DatabaseDialect dialect, Schema schema, boolean key) {
+        Type subType = dialect.getSchemaType(schema.valueSchema());
+        typeName = subType.getTypeName(dialect, schema.valueSchema(), key);
+        return typeName + "[]";
+    }
+
+    @Override
+    public List<ValueBindDescriptor> bind(int index, Schema schema, Object value) {
+        if (value == null) {
+            return Arrays.asList(new ValueBindDescriptor(index, null));
+        }
+        return List.of(new ValueBindDescriptor(index, value, java.sql.Types.ARRAY, typeName));
+    }
+}